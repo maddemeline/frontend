bc4b6e33446a0fbe311791ad34a961990feb7d69
Madeleine Meline <made0613@edu.eal.dk>
2019-03-08 17:51:58 +0100

    Structure of vue project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<script>
+import Header from './components/Header'
+
+
+export default {
+  components: {
+    'app-header': Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>THIS IS THE HEADER with navigation</h1>
+        <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    </div>
+</template>
